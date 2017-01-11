0be0997233214693325d57df388c4be62fcbe3d4
Засыпин Николай Вениаминович <  >
2017-01-11 16:45:55 +0300

    check module parent

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var user = require('./user');
 
+function run(){
 var vasya = new user.User('Vasya');
 var petya = new user.User('Петя');
 
 vasya.hello(petya);
+}
+
+if(module.parent) {
+  exports.run = run;
+} else {
+  run();
+}
