806c4e11da91fc651d90c6b7b67f500f7d2bf585
Засыпин Николай Вениаминович <  >
2017-01-12 10:24:56 +0300

    node_modules, added db module

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var User = require('./user');
+var db = require("db");
+db.connect();
 
 function run(){
 var vasya = new User('Vasya');
 var petya = new User('Петя');
 
 vasya.hello(petya);
+
+console.log(db.getPhrase("Run successful"));
 }
 
 if(module.parent) {
--- user/index.js
+++ user/index.js
@@ -1,13 +1,14 @@
 // module.exports = exports = this
 
-var phrases = require("./ru");
+var db = require("db");
+// db.connect();
 
 function User(name) {
   this.name = name;
 }
 
 User.prototype.hello = function(who) {
-  console.log(phrases.Hello + ", ", who.name);
+  console.log(db.getPhrase("Hello") + ", ", who.name);
 }
 
 console.log("user is required");
